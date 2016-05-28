d4b8d1333d7800dd876e875a7b52742755e355d7
530542319 <1622059450@qq.com>
2016-05-28 21:23:11 +0800

    Updated some methods.

--- ehCacheTest/src/com/aop/ExceptionTest.java
+++ ehCacheTest/src/com/aop/ExceptionTest.java
@@ -3,9 +3,9 @@ package com.aop;
 public class ExceptionTest {
 
 	public static void main(String[] args) {
-		com.toptal.blog.exception.UserExistsException
-		Class<?> clazz;
-		try {
-			clazz = Class.forName(typeName);
+//		com.toptal.blog.exception.UserExistsException
+//		Class<?> clazz;
+//		try {
+//			clazz = Class.forName(typeName);
 	}
 }
--- ehCacheTest/src/com/aop/JsonRpcClientTest.java
+++ ehCacheTest/src/com/aop/JsonRpcClientTest.java
@@ -8,7 +8,7 @@ public class JsonRpcClientTest {
 
 	public static void main(String[] args) throws Throwable {
 		JsonRpcHttpClient client = new JsonRpcHttpClient(new URL(
-				"http://localhost:8080/EhCacheExample-master/UserService.json"));
+				"http://localhost:8080/EhCacheExample/UserService.json"));
 		TbUser user = client.invoke("createUser", new Object[] { "bob",
 				"the builder" }, TbUser.class);
 //		TbUser user1 = client.invoke("findUserByUserName", new Object[] { "bob"}, TbUser.class);
